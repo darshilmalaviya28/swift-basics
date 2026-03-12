dcc050dcab9538af974429d87f3902affface39c
Darshil Malaviya <dgmalaviya2808@gmail.com>
2026-03-12 22:17:47 +0530

    Initial Swift learning project

---
+++ swift-basics/main.swift
@@ -0,0 +1,27 @@
+// Darshil Malaviya — Swift Learning Journey 🚀
+// Day 1 — Variables & Constants
+
+// --------------------------------------
+// MARK: - Variables
+// --------------------------------------
+
+var name = "Darshil"
+var age = 22
+var salary = 25000.0
+
+// --------------------------------------
+// MARK: - Constants
+// --------------------------------------
+
+let city = "Gujarat"
+let language = "Swift"
+
+// --------------------------------------
+// MARK: - Print
+// --------------------------------------
+
+print("Name: \(name)")
+print("City: \(city)")
+print("Language: \(language)")
+print("Current Salary: \(salary)")
+print("Goal: 40000+ 💪")
